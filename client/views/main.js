7944e2348fecf801ec9dc88d76a473d716a6e4ec
gideong <gideong@yahoo-inc.com>
2014-07-24 13:59:34 -0700

    Handling clicks on internal links with sweet events hash.

--- client/views/main.js
+++ client/views/main.js
@@ -5,6 +5,9 @@ var View = require('ampersand-view'),
 module.exports = View.extend({
 	template: templates.body,
 	autoRender: true,
+	events: {
+		'click a[href]': 'handleLinkClick'
+	},
 	initialize: function() {
 		this.listenTo(app.router, 'page', this.handlePage);
 	},
@@ -14,5 +17,14 @@ module.exports = View.extend({
 	},
 	handlePage: function(pageView) {
 		this.pages.set(pageView);
+	},
+	handleLinkClick: function(event) {
+		var aTag = event.target;
+		var isLocal = aTag.host === location.host;
+		var hasModifiers = event.ctrlKey || event.metaKey || event.shiftKey;
+		if (isLocal && !hasModifiers) {
+			event.preventDefault();
+			app.router.history.navigate(aTag.pathname, {trigger: true});
+		}
 	}
 });
