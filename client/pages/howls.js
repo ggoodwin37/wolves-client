c815aa9876856086aafaf862a0c9052f96ba2af0
gideong <gideong@yahoo-inc.com>
2014-07-24 14:59:35 -0700

    Rendering howl list.

--- client/pages/howls.js
+++ client/pages/howls.js
@@ -1,6 +1,14 @@
 var View = require('ampersand-view'),
-	templates = require('../templates');
+	templates = require('../templates'),
+	HowlView = require('../views/howl');
 
 module.exports = View.extend({
-	template: templates.pages.howls()
+	template: templates.pages.howls(),
+	initialize: function() {
+		this.collection = app.howls;
+	},
+	render: function() {
+		this.renderWithTemplate();
+		this.renderCollection(this.collection, HowlView, this.getByRole('howls'));
+	}
 });
