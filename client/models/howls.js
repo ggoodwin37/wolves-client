ec462d5a110e663dde951bc02210a49a125c317c
gideong <gideong@yahoo-inc.com>
2014-07-24 15:24:54 -0700

    Model comparator

--- client/models/howls.js
+++ client/models/howls.js
@@ -3,5 +3,8 @@ var Howl = require('./howl');
 
 module.exports = Collection.extend({
 	model: Howl,
-	url: 'http://wolves.technology/howls'
+	url: 'http://wolves.technology/howls',
+	comparator: function(model) {
+		return -1 * model.createdAt.valueOf;
+	}
 });
