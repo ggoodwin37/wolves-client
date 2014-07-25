a6e8a2a0e3ded44df3e99d9c3eba2a503903a0f9
gideong <gideong@yahoo-inc.com>
2014-07-25 09:53:46 -0700

    Adding a newHowl subview.

--- client/pages/howls.js
+++ client/pages/howls.js
@@ -1,6 +1,7 @@
 var View = require('ampersand-view'),
 	templates = require('../templates'),
-	HowlView = require('../views/howl');
+	HowlView = require('../views/howl'),
+	NewHowlView = require('../views/new-howl');
 
 module.exports = View.extend({
 	template: templates.pages.howls(),
@@ -9,6 +10,11 @@ module.exports = View.extend({
 	},
 	render: function() {
 		this.renderWithTemplate();
+		var newHowlView = new NewHowlView({
+			el: this.getByRole('new-howl')
+		});
+		//newHowlView.render();  // can do this instead of using autorender:true at view construct
+
 		this.renderCollection(this.collection, HowlView, this.getByRole('howls'));
 	}
 });
