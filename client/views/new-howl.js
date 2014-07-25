ed6e8b3c5a42bcd67391cea8b5b0e68fde00cc00
gideong <gideong@yahoo-inc.com>
2014-07-25 10:08:26 -0700

    Submitting howls.

--- client/views/new-howl.js
+++ client/views/new-howl.js
@@ -3,5 +3,15 @@ var templates = require('../templates');
 
 module.exports = View.extend({
 	template: templates.includes.newHowl,
-	autoRender: true
+	autoRender: true,
+	events: {
+		'submit [role=new-howl]': 'createNewHowl'
+	},
+	createNewHowl: function(event) {
+		event.preventDefault();
+		app.howls.create({
+			content: this.get('[name=content]').value,
+			createdAt: new Date()
+		});
+	}
 });
